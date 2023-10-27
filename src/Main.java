847e3939d25ed0d8a0c2451f4ff0d554cf1ef154
Sahbanisahar <sahbani105sahar@gmail.com>
2023-10-27 16:20:53 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Créer un animal (lion) avec des attributs
+        Animal lion = new Animal("Félin", "Lion", 5, true);
+
+        // Créer un zoo (myZoo) avec des attributs
+        Zoo myZoo = new Zoo("Mon Zoo", "VilleZoo", 10);
+
+        // Afficher les détails de base du zoo (name, city, nbrCages)
+        myZoo.displayZoo();
+
+
+
+        }
+    }
---
+++ src/ZooManagement.java
@@ -0,0 +1,13 @@
+//Instruction 3
+import java.util.Scanner;
+
+public class ZooManagement {
+    int nbrCages = 20;
+    String zooName = "belvidair";
+
+    public static void main(String[] args) {
+        //Instruction 1
+        ZooManagement zm = new ZooManagement();
+        System.out.println(zm.zooName + " comporte " + zm.nbrCages);
+    }
+    }
